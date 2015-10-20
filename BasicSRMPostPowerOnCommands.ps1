b816ca7706c3bac42279c144cbd628130c2a53b0
CRad14 <cramos14@gmail.com>
2015-10-20 14:05:17 -0500

    Adding Basic SRM Post Power on Command
    Adding Basic SRM Post Power on Command

---
+++ BasicSRMPostPowerOnCommands.ps1
@@ -0,0 +1,54 @@
+﻿add-pssnapin vmware.vimautomation.core 
+Import-Module E:\scripts\Meadowcroft.SRM\Meadowcroft.Srm.psm1 
+ 
+ 
+ 
+
+ 
+Connect-viserver "vCenterServer"
+ 
+Connect-srmserver 
+ 
+$scriptblock={Ipconfig /all}
+ 
+ 
+ 
+$srmcommand=new-command -command $scriptblock -description "WinRecovery" -runinrecoveredvm -timeout 300 
+
+ 
+ 
+#Grabs All SRM Plan
+$plans=Get-RecoveryPlan  
+ 
+FOREACH ($plan in $plans) 
+{ 
+
+#Retrieves all VMs from each Plan
+$winvms=$plan |get-protectedvm |? {($_.vm.guest.guestfamily -like "*win*")} 
+ 
+ 
+FOREACH ($winvm in $winvms) 
+ 
+{ 
+$vmname=$winvm.vm.name 
+$vmname 
+
+
+$srmpostcommand=$srmcommand
+ 
+$settings=get-recoverysettings -ProtectedVm $winvm -RecoveryPlan $plan 
+ 
+#Retrieves Current PostPower On Commands
+$postcommands=$settings.postpoweroncallouts |% { $_ } 
+
+#Remove All Currently assigned Recovery Commands
+ForEach ($postcommand in $postcommands) 
+{remove-postrecoverycommand -recoverysettings $settings -command $postcommand} 
+ 
+#Sets the Recovery Commands 
+Add-PostRecoveryCommand -RecoverySettings $settings -command $srmpostcommand 
+ 
+Set-RecoverySettings -RecoveryPlan $plan -ProtectedVm $winvm -RecoverySettings $settings 
+ 
+} 
+}
