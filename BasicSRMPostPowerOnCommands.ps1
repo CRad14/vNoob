01a4e2586f46c56c69b4d5976da4699c8f40b336
Conrad Ramos <cramos14@gmail.com>
2024-11-03 06:07:18 -0600

    Update BasicSRMPostPowerOnCommands.ps1
    Tossed in chatgpt to add comments and formatting

--- BasicSRMPostPowerOnCommands.ps1
+++ BasicSRMPostPowerOnCommands.ps1
@@ -1,54 +1,57 @@
-﻿add-pssnapin vmware.vimautomation.core 
-Import-Module E:\scripts\Meadowcroft.SRM\Meadowcroft.Srm.psm1 
- 
- 
- 
+# Add VMware PowerCLI Snap-in
+add-pssnapin vmware.vimautomation.core
 
- 
+# Import custom module for SRM automation
+Import-Module E:\scripts\Meadowcroft.SRM\Meadowcroft.Srm.psm1
+
+# Connect to vCenter Server
 Connect-viserver "vCenterServer"
- 
-Connect-srmserver 
- 
-$scriptblock={Ipconfig /all}
- 
- 
- 
-$srmcommand=new-command -command $scriptblock -description "WinRecovery" -runinrecoveredvm -timeout 300 
 
- 
- 
-#Grabs All SRM Plan
-$plans=Get-RecoveryPlan  
- 
-FOREACH ($plan in $plans) 
-{ 
+# Connect to SRM Server
+Connect-srmserver
 
-#Retrieves all VMs from each Plan
-$winvms=$plan |get-protectedvm |? {($_.vm.guest.guestfamily -like "*win*")} 
- 
- 
-FOREACH ($winvm in $winvms) 
- 
-{ 
-$vmname=$winvm.vm.name 
-$vmname 
+# Define a script block to retrieve network configuration for the VM
+$scriptblock = { Ipconfig /all }
+
+# Create a new SRM command for running in the recovered VM with a timeout of 300 seconds
+$srmcommand = New-Command -command $scriptblock -description "WinRecovery" -runinrecoveredvm -timeout 300
+
+# Get all SRM Recovery Plans
 
 
-$srmpostcommand=$srmcommand
- 
-$settings=get-recoverysettings -ProtectedVm $winvm -RecoveryPlan $plan 
- 
-#Retrieves Current PostPower On Commands
-$postcommands=$settings.postpoweroncallouts |% { $_ } 
+$plans = Get-RecoveryPlan
 
-#Remove All Currently assigned Recovery Commands
-ForEach ($postcommand in $postcommands) 
-{remove-postrecoverycommand -recoverysettings $settings -command $postcommand} 
- 
-#Sets the Recovery Commands 
-Add-PostRecoveryCommand -RecoverySettings $settings -command $srmpostcommand 
- 
-Set-RecoverySettings -RecoveryPlan $plan -ProtectedVm $winvm -RecoverySettings $settings 
- 
-} 
+# Loop through each Recovery Plan
+FOREACH ($plan in $plans) {
+
+    # Retrieve all Windows VMs from the Recovery Plan
+    $winvms = $plan | Get-ProtectedVM | Where-Object { ($_.vm.guest.guestfamily -like "*win*") }
+
+    # Loop through each Windows VM in the plan
+    FOREACH ($winvm in $winvms) {
+
+        # Get the name of the VM
+        $vmname = $winvm.vm.name
+        $vmname
+
+        # Set the SRM post-recovery command
+        $srmpostcommand = $srmcommand
+
+        # Retrieve current recovery settings for the VM
+        $settings = Get-RecoverySettings -ProtectedVm $winvm -RecoveryPlan $plan
+
+        # Retrieve all current post-power-on commands
+        $postcommands = $settings.postpoweroncallouts | ForEach-Object { $_ }
+
+        # Remove all currently assigned recovery commands
+        ForEach ($postcommand in $postcommands) {
+            Remove-PostRecoveryCommand -RecoverySettings $settings -command $postcommand
+        }
+
+        # Add the new recovery command to the post-power-on commands
+        Add-PostRecoveryCommand -RecoverySettings $settings -command $srmpostcommand
+
+        # Apply the updated recovery settings to the recovery plan
+        Set-RecoverySettings -RecoveryPlan $plan -ProtectedVm $winvm -RecoverySettings $settings
+    }
 }
